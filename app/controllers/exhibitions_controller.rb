ba127e83cbfd0fd9bdeb031848828009d230f8a1
Ekaterina Shemerey <rooh@mail.ru>
2015-03-10 01:32:44 +0300

    price in rubles

--- app/controllers/exhibitions_controller.rb
+++ app/controllers/exhibitions_controller.rb
@@ -1,7 +1,19 @@
 class ExhibitionsController < ApplicationController
 
   def index
-    @exhibitions = Exhibition.all
+    if params[:type] == 'Aquarelle'
+      @exhibitions = Exhibition.where(type: "Aquarelle")
+    elsif params[:type] == 'Oil'
+      @exhibitions = Exhibition.where(type: "Oil")
+    elsif params[:type] == 'Grattage'
+      @exhibitions = Exhibition.where(type: "Grattage")
+    elsif params[:type] == 'Gouache'
+      @exhibitions = Exhibition.where(type: "Gouache")
+    elsif params[:type] == 'Graphics'
+      @exhibitions = Exhibition.where(type: "Graphics")
+    else
+      @exhibitions = Exhibition.all
+    end
   end
 
   def show
--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -1,3 +1,7 @@
 class Exhibition < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
+
+  monetize :price_cents, :as => "price", :with_currency => :rub
+  register_currency :rub
+
 end
