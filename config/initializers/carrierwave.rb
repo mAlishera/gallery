bc27f083cbbb7974a24d237dcb8146cf014d73af
Ekaterina Shemerey <rooh@mail.ru>
2015-03-16 20:29:10 +0300

    ass

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
   config.aws_credentials = {
-    access_key_id:     ENV[:AWS_ACCESS_KEY_ID],
-    secret_access_key: ENV[:AWS_SECRET_ACCESS_KEY]
+    access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+    secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
   }
 end
