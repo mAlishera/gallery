2b91c6c9fbb53436089a1e72a515b6ecfc329ea6
Ekaterina Shemerey <rooh@mail.ru>
2015-03-16 12:17:59 +0300

    assets try7

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,8 @@ module Gallery
           end
         end
 
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'uploads')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
