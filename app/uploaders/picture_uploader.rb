db60245ca8942cd41cae0757dc37569126bde82c
Ekaterina Shemerey <rooh@mail.ru>
2015-03-16 21:11:50 +0300

    resized

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -31,6 +31,15 @@ class PictureUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
+   version :resized do
+      # returns an image with a maximum width of 100px
+      # while maintaining the aspect ratio
+      # 10000 is used to tell CW that the height is free
+      # and so that it will hit the 100 px width first
+      process :resize_to_limit => [300, 10000]
+    end
+
+
   # Create different versions of your uploaded files:
   # version :thumb do
   #   process :resize_to_fit => [50, 50]
