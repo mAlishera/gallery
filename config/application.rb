c46cbb933c61fbd9567cf0b4d2a106d50db21eba
Ekaterina Shemerey <rooh@mail.ru>
2015-03-16 12:11:40 +0300

    assets try6

--- config/application.rb
+++ config/application.rb
@@ -2,36 +2,36 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif,
-                                  "fontawesome-webfont.ttf",
-                                 "fontawesome-webfont.eot",
-                                 "fontawesome-webfont.svg",
-                                 "fontawesome-webfont.woff")
-
-
-
-config.assets.precompile << Proc.new do |path|
-      if path =~ /\.(css|js)\z/
-        full_path = Rails.application.assets.resolve(path).to_path
-        app_assets_path = Rails.root.join('app', 'assets').to_path
-        if full_path.starts_with? app_assets_path
-          puts "including asset: " + full_path
-          true
-        else
-          puts "excluding asset: " + full_path
-          false
-        end
-      else
-        false
-      end
-    end
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Gallery
   class Application < Rails::Application
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif,
+                                      "fontawesome-webfont.ttf",
+                                     "fontawesome-webfont.eot",
+                                     "fontawesome-webfont.svg",
+                                     "fontawesome-webfont.woff")
+
+
+
+    config.assets.precompile << Proc.new do |path|
+          if path =~ /\.(css|js)\z/
+            full_path = Rails.application.assets.resolve(path).to_path
+            app_assets_path = Rails.root.join('app', 'assets').to_path
+            if full_path.starts_with? app_assets_path
+              puts "including asset: " + full_path
+              true
+            else
+              puts "excluding asset: " + full_path
+              false
+            end
+          else
+            false
+          end
+        end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
